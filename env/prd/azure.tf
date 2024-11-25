51141b785a8e7ad1bea44a87ac8c6ac76f3cb690
Joerg Kritzen <j.kritzen@ontracon.de>
2024-11-25 19:02:55 +0100

    fix tflint

--- env/prd/azure.tf
+++ env/prd/azure.tf
@@ -33,5 +33,13 @@ module "vnet" {
   }
 }
 
+# Creating RG's for Pools VM
+module "resource_group" {
+  for_each = toset(local.azure_locations)
+  source       = "git::https://github.com/otc-code/res-azr-rg.git?ref=v1.0.0"
+  cloud_region = each.key
+  config       = var.config
+  custom_name = "vm"
+}
 
 
