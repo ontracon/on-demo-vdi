7442b632587ff01a4680ef37e0edf28130732697
Joerg Kritzen <j.kritzen@ontracon.de>
2024-11-25 14:05:55 +0100

    add azure deployment

--- env/prd/azure.tf
+++ env/prd/azure.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
 # Prepare Base Infrastructure for Azure Windows VM's
 locals {
   azure_locations=["germanywestcentral"]
@@ -9,6 +16,7 @@ module "resource_group" {
   cloud_region = each.key
   config       = var.config
 }
+
 
 
 
